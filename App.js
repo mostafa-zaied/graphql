0ea8ba8759c215df3caa2b103209b1cdc743af70
mostafa_zaied <Maly@ntgclarity.com>
2020-04-21 13:21:45 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button } from 'react-native';
+ 
+export const Example = () => {
+  const [foo, setFoo] = useState(30);
+ 
+  useEffect(() => {
+    if (foo >= 42) {
+      setFoo(42);
+    }
+  }, [foo])
+ 
+  return (
+    <View style={{flex:1}}> 
+      <Text style={{padding:50}}>Foo is {foo}.</Text>
+      <Button onPress={() => setFoo(foo + 1)} title='Increase Foo!' />
+    </View>
+  )
+}
+export default Example;
